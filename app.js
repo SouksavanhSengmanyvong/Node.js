e25990f75586f3ce99d30b3e9a2e4abaf77a4634
SouksavanhSengmanyvong <136735396+SouksavanhSengmanyvong@users.noreply.github.com>
2023-07-19 18:16:50 +0700

    Add all files of Project
    Hello world:100:

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const chalk = require('chalk')
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+app.get("/", (req,res)=>{
+
+res.send('Hello Kevin Co., Ltd.');
+
+})
+
+app.listen(port, ()=>{
+debug("listening on port" + chalk.green(port))
+})
